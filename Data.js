f7e23baf0571dbbc82ad7ac3708b93b15ba4d2d9
AmirDanesh <daneshahd78@gmail.com>
2023-03-09 21:13:56 +0330

    rebuild projects and use local Data

---
+++ Data.js
@@ -0,0 +1,167 @@
+const projects = {
+    "Projects": {
+        "-NE5vdTQpb46nmvS1FKF": {
+            "ProjectLink": "./CtoF",
+            "ProjectName": "CtoF",
+            "ProjectNumber": "1",
+            "ProjectStatus": "Finished"
+        },
+        "-NE5w_ugrJaRBB92LnAK": {
+            "ProjectLink": "./TodoList",
+            "ProjectName": "TodoList",
+            "ProjectNumber": "2",
+            "ProjectStatus": "Finished"
+        },
+        "-NE5x0PNBWC1_7mYZnkm": {
+            "ProjectLink": "./customContextMenu",
+            "ProjectName": "CustomContextMenu",
+            "ProjectNumber": "3",
+            "ProjectStatus": "Finished"
+        },
+        "-NE5xWq9thR4C3PYAZKA": {
+            "ProjectLink": "./preLoad",
+            "ProjectName": "PreLoad",
+            "ProjectNumber": "4",
+            "ProjectStatus": "Finished"
+        },
+        "-NE5xm9e3Uswn64BhbPw": {
+            "ProjectLink": "./InputMaxLenght",
+            "ProjectName": "InputMaxLenght",
+            "ProjectNumber": "5",
+            "ProjectStatus": "Finished"
+        },
+        "-NE5xy-LWSxYSTCBUsNC": {
+            "ProjectLink": "./noteApp",
+            "ProjectName": "NoteApp",
+            "ProjectNumber": "6",
+            "ProjectStatus": "Finished"
+        },
+        "-NE5yM1X_-bJIEYWHyHo": {
+            "ProjectLink": "./stickyNavbar",
+            "ProjectName": "StickyNavbar",
+            "ProjectNumber": "7",
+            "ProjectStatus": "Finished"
+        },
+        "-NE5yTk6tzQn8hIQAVHi": {
+            "ProjectLink": "./musicPlayer",
+            "ProjectName": "MusicPlayer",
+            "ProjectNumber": "8",
+            "ProjectStatus": "Finished"
+        },
+        "-NE5y_JA69OEWY2CQ8VA": {
+            "ProjectLink": "./MusicList",
+            "ProjectName": "MusicList",
+            "ProjectNumber": "9",
+            "ProjectStatus": "Finished"
+        },
+        "-NE5ylPypQLyZACQxmgw": {
+            "ProjectLink": "./DarkMode",
+            "ProjectName": "DarkMode",
+            "ProjectNumber": "10",
+            "ProjectStatus": "Finished"
+        },
+        "-NE5yqOho0ysdSuqgN60": {
+            "ProjectLink": "./TodoListLocal",
+            "ProjectName": "TodoListLocal",
+            "ProjectNumber": "11",
+            "ProjectStatus": "Finished"
+        },
+        "-NE5yvFjtT2_ZKuGX9YW": {
+            "ProjectLink": "./Brightness",
+            "ProjectName": "Brightness",
+            "ProjectNumber": "12",
+            "ProjectStatus": "Finished"
+        },
+        "-NE5z9ta7_Ju-zE6v_n6": {
+            "ProjectLink": "./imageSlider",
+            "ProjectName": "ImageSlider",
+            "ProjectNumber": "13",
+            "ProjectStatus": "Finished"
+        },
+        "-NE5zEJ3T4HAbgq9s5-2": {
+            "ProjectLink": "./BmiCal",
+            "ProjectName": "BmiCal",
+            "ProjectNumber": "14",
+            "ProjectStatus": "Finished"
+        },
+        "-NE5zIvhwmrsBiI5LrTA": {
+            "ProjectLink": "./changeTheme",
+            "ProjectName": "ChangeTheme",
+            "ProjectNumber": "15",
+            "ProjectStatus": "Finished"
+        },
+        "-NE5zSyDaNavAw1Pu7ei": {
+            "ProjectLink": "./KeyboardAnimate",
+            "ProjectName": "KeyboardAnimate",
+            "ProjectNumber": "16",
+            "ProjectStatus": "Finished"
+        },
+        "-NE5z_7xo248AWRWm0Yt": {
+            "ProjectLink": "./BookListLocalS",
+            "ProjectName": "BookListLocalS",
+            "ProjectNumber": "17",
+            "ProjectStatus": "Finished"
+        },
+        "-NE5zfROjEqbjuY4_Hm7": {
+            "ProjectLink": "./DigitalClock",
+            "ProjectName": "DigitalClock",
+            "ProjectNumber": "18",
+            "ProjectStatus": "Finished"
+        },
+        "-NE5zkzhFrXVZO02E22w": {
+            "ProjectLink": "./listPagination",
+            "ProjectName": "ListPagination",
+            "ProjectNumber": "19",
+            "ProjectStatus": "Finished"
+        },
+        "-NE6-ZW2L4nqvfaKVaQT": {
+            "ProjectLink": "./CustomScrollBar",
+            "ProjectName": "CustomScrollBar",
+            "ProjectNumber": "20",
+            "ProjectStatus": "Finished"
+        },
+        "-NE6-fa0pQwLPW3pacG7": {
+            "ProjectLink": "./shoesShop",
+            "ProjectName": "ShoesShop",
+            "ProjectNumber": "21",
+            "ProjectStatus": "Finished"
+        },
+        "-NE6-jpums9r4wKkzCik": {
+            "ProjectLink": "./filterSearchBar",
+            "ProjectName": "FilterSearchBar",
+            "ProjectNumber": "22",
+            "ProjectStatus": "Finished"
+        },
+        "-NE6-qRO74ZGnWpNsh1D": {
+            "ProjectLink": "./LoginPageIndexDB/register.html",
+            "ProjectName": "LoginPageIndexDB",
+            "ProjectNumber": "23",
+            "ProjectStatus": "Finished"
+        },
+        "-NE6-udfw5W_uOydayFA": {
+            "ProjectLink": "./RegisterPage",
+            "ProjectName": "RegisterPage",
+            "ProjectNumber": "24",
+            "ProjectStatus": "Finished"
+        },
+        "-NE6-yTrWPXDsJ4P-ZTQ": {
+            "ProjectLink": "./WeatherApi",
+            "ProjectName": "WeatherApi",
+            "ProjectNumber": "25",
+            "ProjectStatus": "Finished"
+        },
+        "-NE60ppiX1TbBeaP6tQa": {
+            "ProjectLink": "https://bestwebneed.ir/Cv/CvProject/Public/JsProject/AppLab/",
+            "ProjectName": "AppLab",
+            "ProjectNumber": "26",
+            "ProjectStatus": "Finished"
+        },
+        "-NE616HTa5PUHcYeiB6S": {
+            "ProjectLink": "https://bestwebneed.ir/Cv/CvProject/Public/JsProject/Notepad/",
+            "ProjectName": "Notepad",
+            "ProjectNumber": "27",
+            "ProjectStatus": "Finished"
+        }
+    }
+}
+
